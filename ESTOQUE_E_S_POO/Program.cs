4a0aac22c4ea2f54088e505fdb9a4f1d1fb1d1a1
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-12 16:00:00 -0300

    primeiro commit

---
+++ ESTOQUE_E_S_POO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ESTOQUE_E_S_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {produto}");
+            Console.WriteLine();
+           
+            Console.Write("Digite o número de produtos a ser adicionado no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {produto}");
+            Console.WriteLine();
+            
+            Console.Write("Digite o número de produtos a ser retirado no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qte);
+            Console.WriteLine($"Dados atualizados: {produto}");
+        }
+    }
+}
